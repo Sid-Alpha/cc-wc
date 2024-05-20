34e7c0d5460e5beb7e8c82cf3e4688d0f93e7f1e
koppikar <koppikar@sid.myguest.virtualbox.org>
2024-05-20 17:13:41 +0200

    Support for counting lines with a -l flag

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,42 @@
-use std::env;
+// use std::env;
 use std::fs;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Bytes
+    #[arg(short='c')]
+    bytes: bool,
+
+    /// Number of times to greet
+    #[arg(short='l')]
+    lines: bool,
+
+    file: String,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    // dbg!(args);
-    let file = &args[1];
+    let args: Args = Args::parse();
+    let file = args.file;
+    let mut res: String = "".to_owned();
+    if args.bytes {
+        let vec = fs::read(&file).expect("Should have been able to read the file");
+        let size = vec.len(); //metadata().unwrap.len();
+        res.push_str(size.to_string().as_str());
+        res.push_str(" bytes");
+    }
+    if args.lines {
+        let mut lines = 0;
+        let file_str = fs::read_to_string(&file).expect("Should have been able to read the file");
+        for _line in file_str.lines() {
+            lines += 1;
+        }
+        let lines_op = format!(" {lines} lines");
+        res.push_str(&*lines_op);
+    }
 
-    println!("File path: {}", file);
-    let vec = fs::read(file).expect("Should have been able to read the file");
-    // let size = content.metadata().unwrap.len();
-    println!("{}", vec.len());
-    // println!("println/zHello, world!");
-
+    res.push_str(" ");
+    res.push_str(file.as_str());
+    println!(" {}", res);
 }
